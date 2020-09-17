dd2f48b752739a2ec864e50d1b5b0932262dafdb
Ayush Kumar <a.ayushkumar1997@gmail.com>
2020-09-17 13:33:49 +0530

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -9,7 +9,7 @@ echo ------------------------------------------------------------Instructions to
 echo 1. nusmv -int fileName
 echo 2. go 
 echo 3. check_ltlspec or check_ctlspec depending upon if your are using LTL or CTL
-echo 4. show_traces 2 -0 fileName
+echo 4. show_traces 2 -o fileName
 echo 5. quit
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo If you find any issues with the above commands or if you want to update the current syntaxes please make a pull request with the changes to https://github.com/ayush4190/NusmVInstallation.git so that everyone finds it useful
